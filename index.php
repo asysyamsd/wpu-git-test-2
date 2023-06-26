22e0bd61ce4182db0b27eb024dff4840a705dcc7
asysyamsd <darussalamasysyams@gmail.com>
2023-06-26 07:18:39 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Coba remote git</title>
-  </head>
-  <body>
+</head>
+
+<body>
     <h1>Hello World!</h1>
-  </body>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Inventore eum, quod modi voluptas sapiente excepturi?
+        Laborum eos minus accusantium quia?</p>
+</body>
+
 </html>
